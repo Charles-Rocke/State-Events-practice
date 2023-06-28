4cd00e106a25bfab9d79651755bd5dd5ad4528cb
SpOOk <spook.devv@gmail.com>
2023-06-28 19:31:07 -0400

    Added children props

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ export default function App() {
   return (
     <div>
       <Steps />
-      <Steps />
+
+      {/* reusable component */}
+      {/* <StepMessage step={1}>
+        <p>Pass in content</p>
+      </StepMessage> */}
+      {/* <Steps /> */}
     </div>
   );
 }
@@ -44,25 +49,53 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                text="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn How
+              </Button>
+            </div>
+          </StepMessage>
+
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+            {/* Children go inbetween the ...>children here</... */}
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+// button component
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+// Step message component
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
